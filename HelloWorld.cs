a5f2ad3086310a37d4a7f2ec00d3cb9c17e0770c
RubencinGuitarrista <156347848+RubencinGuitarrista@users.noreply.github.com>
2024-01-11 17:48:53 +0100

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;  
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Se escribe en la pantalla el texto que esta dentro del print
+        print("Hello World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
