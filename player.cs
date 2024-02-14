adc62fa5746721c2d3fa81943ad68ae606f4b57d
RubencinGuitarrista <156347848+RubencinGuitarrista@users.noreply.github.com>
2024-02-14 18:11:34 +0100

    Update player.cs

--- player.cs
+++ player.cs
@@ -1,1 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;  
+using UnityEngine;
 
+public class player : MonoBehaviour
+{
+  public GameObject fireballPrefab
+  public Transform attackPoint
+
+  if (Input.GetMouseButtonDown(0))
+  {
+      Instantiate(fireballPrefab, attackPoint.position, attackPoint.rotation);
+  }
+}
